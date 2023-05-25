80eae3456dd827ad78be96d81d0e195d9156ca4c
MiniManch <Emaneulrokah96@gmail.com>
2023-05-25 20:09:28 +0300

    changing to debug-False

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config:
 	SECRET_KEY = os.environ['SECRET_KEY']
 	UPLOAD_FOLDER = 'static/uploaded_images'
 	# SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{os.environ["MYSQL_USER"]}:{os.environ["MYSQL_PASSWORD"]}@{os.environ["MYSQL_DATABASE"]}?charset=utf8mb4'
-	SQLALCHEMY_DATABASE_URI = 'postgresql://finalproject_u06w_user:M0G4svlSPLokrdSWFFxfCwy6FK8SI0e1@dpg-chnp1482qv207f235afg-a/finalproject_u06w'
+	SQLALCHEMY_DATABASE_URI = 'postgresql://finalproject_u06w_user:M0G4svlSPLokrdSWFFxfCwy6FK8SI0e1@dpg-chnp1482qv207f235afg-a.frankfurt-postgres.render.com/finalproject_u06w'
 
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = 'smtp.gmail.com'
